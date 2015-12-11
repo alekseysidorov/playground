9cfff6e5a5b0fdabda45c365b9b003016341b026
Aleksey Sidorov <alexey.sidorov@opensoftdev.ru>
2015-12-11 19:43:00 +0300

    Add Pixmap implementation to swift code

--- swift_vs_rust/raster/main.swift
+++ swift_vs_rust/raster/main.swift
@@ -9,4 +9,39 @@ struct GenericPixmap<T> {
         self.h = height
         self.data = [T](count: w*h, repeatedValue: fillValue)
     }
+
+    func indexIsValid(x: Int, _ y: Int) -> Bool {
+        return x >= 0 && x < w && y >= 0 && y < h
+    }
+
+    subscript(x: Int, y: Int) -> T {
+        get {
+            assert(indexIsValid(x, y))
+            return data[x * y + y]
+        }
+        set {
+            assert(indexIsValid(x,y))
+            data[x * y + y] = newValue
+        }
+    }
+}
+
+typealias Pixmap = GenericPixmap<UInt32>
+
+protocol Canvas {
+    func setPixel(x: Int, y: Int, color: UInt32);
+}
+
+class MyCanvas : Canvas
+{
+    var pixmap: Pixmap
+    
+    init(width: Int, height: Int, fillValue: UInt32) {
+        self.pixmap = Pixmap(width:width, height:height, fillValue:fillValue)
+    }
+    
+    func setPixel(x: Int, y: Int, color: UInt32)
+    {
+        pixmap[x, y] = color
+    }
 }
