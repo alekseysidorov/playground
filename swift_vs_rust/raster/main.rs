c781c3a73e89c44e62c092b488af34efd3c981f5
Aleksey Sidorov <gorthauer87@ya.ru>
2015-12-13 15:47:44 +0300

    LineRasterizer for Rust

--- swift_vs_rust/raster/main.rs
+++ swift_vs_rust/raster/main.rs
@@ -56,6 +56,7 @@ impl Canvas for Pixmap
     }
 }
 
+#[derive(Copy, Clone, PartialEq)]
 struct Vec3 {
     x: i32,
     y: i32,
@@ -87,7 +88,91 @@ impl IndexMut<usize> for Vec3
     }    
 }
 
+struct RasterState {
+    step: Vec3,
+    d: Vec3,
+    major_axis: usize,
+}
+
+struct LineRasterizer {
+    from: Vec3,
+    to: Vec3,
+
+    state: Option<RasterState>
+}
+
+impl LineRasterizer {
+
+    fn new(from: Vec3, to: Vec3) -> LineRasterizer {
+        LineRasterizer {
+            from: from,
+            to: to,
+            state: None
+        }
+    }
+
+    fn next_point(&mut self) -> Option<Vec3> {
+        match self.state {
+            None => {
+                let mut state = RasterState {
+                    step: Vec3 { x: 0, y: 0, z: 0 },
+                    d: Vec3 { x: 0, y: 0, z: 0 },
+                    major_axis: 0
+                };   
+
+                let mut max = 0;
+                for i in 0..3 {
+                    let d = self.to[i] - self.from[i];
+                    state.step[i] = if d > 0 { 1 } else { -1 };
+                    
+                    let d = d.abs();
+                    if d > max {
+                        max = d;
+                        state.major_axis = i as usize;
+                    };
+                }
+
+                self.state = Some(state);
+                Some(self.from)
+            },
+            Some(ref mut state) => {
+                if self.from == self.to {
+                    None
+                } else {
+                    let from = self.from; let to = self.to;
+                    let calc_residual_steps = |axis| { (to[axis] - from[axis]).abs() };
+                    
+                    self.from[state.major_axis] += state.step[state.major_axis];                    
+                    let rs_base = calc_residual_steps(state.major_axis);
+                    for i in 0..3 {
+                        let rs = calc_residual_steps(i);
+                        
+                        if rs > 0 && i != state.major_axis {
+                            state.d[i] += rs;
+                            if state.d[i] >= rs_base {
+                                state.d[i] -= rs_base;
+                                self.from[i] += state.step[i];
+                            }
+                        }
+                    }                    
+
+                    Some(self.from)
+                }
+            },
+        }
+    }
+}
 
 fn main() {
-    let canvas = Pixmap::new(300, 300, 0);
+    let mut canvas = Pixmap::new(300, 300, 0);
+
+    let a = Vec3 { x: 0, y:0, z:0 };
+    let b = Vec3 { x: 10, y: 5, z: -4 };
+
+    let mut rasterizer = LineRasterizer::new(a, b);
+    while let Some(point) = rasterizer.next_point() {
+        let color = std::u32::MAX;
+        canvas.set_pixel(point.x as usize, point.y as usize, color);
+        println!("point: x: {}, y: {}, z: {}, color: #{:X}", point.x, point.y, point.z, color);
+    }
 }
