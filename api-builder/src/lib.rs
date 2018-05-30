4c9b1741077d588b8574cdb6444e293577236efc
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2018-05-30 16:17:41 +0300

    PoC for endpoint builder

---
+++ api-builder/src/lib.rs
@@ -0,0 +1,100 @@
+extern crate actix_web;
+extern crate serde;
+extern crate serde_json;
+extern crate failure;
+
+use actix_web::{Scope, Query, FromRequest};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
+use std::io;
+
+#[derive(Clone)]
+pub struct ApiContext {
+    pub name: String,
+}
+
+#[derive(Clone)]
+pub struct ApiContextMut {
+    pub count: u64,
+}
+
+pub enum ApiScope {
+    Public,
+    Internal,
+}
+
+pub struct EndpointIdentifier {
+    pub name: &'static str,
+}
+
+pub trait ReadEndpoint: 'static {
+    const ID: EndpointIdentifier;
+
+    type Request: DeserializeOwned;
+    type Response: Serialize;
+
+    fn handle(
+        &self,
+        context: &ApiContext,
+        request: &Self::Request,
+    ) -> Result<Self::Response, failure::Error>;
+}
+
+pub trait WriteEndpoint {
+    const ID: EndpointIdentifier;
+
+    type Request: DeserializeOwned;
+    type Response: Serialize;
+
+    fn handle(
+        &self,
+        context: &ApiContextMut,
+        request: &Self::Request,
+    ) -> Result<Self::Response, failure::Error>;
+}
+
+pub struct ApiBuilder {
+    scope: Scope<ApiContext>,
+}
+
+impl ApiBuilder {
+    pub fn new(scope: Scope<ApiContext>) -> ApiBuilder {
+        ApiBuilder {
+            scope: Scope::new(),
+        }
+    }
+
+    pub fn for_service<F>(mut self, name: &str, builder: F) -> Self
+    where
+        F: FnOnce(ServiceApiBuilder) -> ServiceApiBuilder,
+    {
+        let f = |scope: Scope<ApiContext>| -> Scope<ApiContext> {
+            builder(ServiceApiBuilder { scope }).scope
+        };
+        self.scope = self.scope.nested(name, f);
+        self
+    }
+
+    pub fn into_scope(self) -> Scope<ApiContext> {
+        self.scope
+    }
+}
+
+pub struct ServiceApiBuilder {
+    scope: Scope<ApiContext>,
+}
+
+impl ServiceApiBuilder {
+    pub fn read_endpoint<E: ReadEndpoint>(mut self, endpoint: E) -> ServiceApiBuilder {
+        let index = move |request: actix_web::HttpRequest<ApiContext>| -> actix_web::Result<String> {
+            let state = request.state();
+            let query = Query::from_request(&request, &())?;
+            let response = endpoint.handle(state, &query)?;
+            serde_json::to_string(&response).map_err(From::from)
+        };
+
+        self.scope = self.scope.route(E::ID.name, actix_web::http::Method::GET, index);
+        self
+    }
+}
---
+++ api-builder/src/main.rs
@@ -0,0 +1,77 @@
+extern crate actix_web;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate api_builder;
+extern crate failure;
+extern crate serde_urlencoded;
+
+use actix_web::*;
+use api_builder::*;
+
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug, Deserialize)]
+pub struct MyRequest {
+    pub name: String,
+    pub count: u64,
+}
+
+#[derive(Debug, Serialize)]
+pub struct MyResponse(String);
+
+pub struct MyConcreteEndpoint;
+pub struct MyConcreteEndpoint2;
+
+impl ReadEndpoint for MyConcreteEndpoint {
+    const ID: EndpointIdentifier = EndpointIdentifier { name: "foo" };
+
+    type Request = MyRequest;
+    type Response = MyResponse;
+
+    fn handle(
+        &self,
+        context: &ApiContext,
+        request: &Self::Request,
+    ) -> Result<Self::Response, failure::Error> {
+        let string = format!(
+            "Context value: {} for name: {} with count: {}",
+            context.name, request.name, request.count
+        );
+        Ok(MyResponse(string))
+    }
+}
+
+impl ReadEndpoint for MyConcreteEndpoint2 {
+    const ID: EndpointIdentifier = EndpointIdentifier { name: "boo" };
+
+    type Request = ();
+    type Response = String;
+
+    fn handle(
+        &self,
+        context: &ApiContext,
+        request: &Self::Request,
+    ) -> Result<Self::Response, failure::Error> {
+        Ok("Hello actix".to_owned())
+    }
+}
+
+fn main() {
+    server::new(|| {
+        let state = ApiContext {
+            name: "context".to_owned(),
+        };
+        App::with_state(state).scope("/api", |scope| {
+            ApiBuilder::new(scope)
+                .for_service("rustfest", |api_builder: ServiceApiBuilder| {
+                    api_builder
+                        .read_endpoint(MyConcreteEndpoint)
+                        .read_endpoint(MyConcreteEndpoint2)
+                })
+                .into_scope()
+        })
+    }).bind("localhost:8080")
+        .unwrap()
+        .run()
+}
