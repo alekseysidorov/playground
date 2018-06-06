1884e1c705186c71935086f444f2e8023bb7f37b
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2018-06-06 13:37:06 +0300

    Polish code

--- api-builder/src/actix_backend.rs
+++ api-builder/src/actix_backend.rs
@@ -3,9 +3,8 @@ use futures::{Future, IntoFuture};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
-use error;
 use service::{ServiceApiBackend, ServiceApiContext, ServiceApiContextMut};
-use NamedFn;
+use {AsyncResult, NamedFn, Result};
 
 pub type RawHandler = Fn(HttpRequest<ServiceApiContextMut>)
     -> Box<Future<Item = HttpResponse, Error = actix_web::Error>>;
@@ -14,135 +13,6 @@ pub struct RequestHandler {
     pub name: &'static str,
     pub method: actix_web::http::Method,
     pub inner: Box<RawHandler>,
-}
-
-impl<Q, I, F> From<NamedFn<ServiceApiContext, Q, I, Result<I, error::Error>, F>> for RequestHandler
-where
-    F: for<'r> Fn(&'r ServiceApiContext, Q) -> Result<I, error::Error> + 'static,
-    Q: DeserializeOwned + 'static,
-    I: Serialize + 'static,
-{
-    fn from(f: NamedFn<ServiceApiContext, Q, I, Result<I, error::Error>, F>) -> Self {
-        let handler = f.inner.f;
-        let index = move |request: HttpRequest<ServiceApiContextMut>|
-         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
-            let context = request.state();
-            let future = Query::from_request(&request, &())
-                .map(|query: Query<Q>| query.into_inner())
-                .and_then(|query| handler(context, query).map_err(From::from))
-                .and_then(|value| Ok(HttpResponse::Ok().json(value)))
-                .into_future();
-            Box::new(future)
-        };
-
-        RequestHandler {
-            name: f.name,
-            method: actix_web::http::Method::GET,
-            inner: Box::new(index) as Box<RawHandler>,
-        }
-    }
-}
-
-impl<Q, I, F> From<NamedFn<ServiceApiContextMut, Q, I, Result<I, error::Error>, F>>
-    for RequestHandler
-where
-    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> Result<I, error::Error> + 'static + Clone,
-    Q: DeserializeOwned + 'static,
-    I: Serialize + 'static,
-{
-    fn from(f: NamedFn<ServiceApiContextMut, Q, I, Result<I, error::Error>, F>) -> Self {
-        let handler = f.inner.f;
-        let index = move |request: HttpRequest<ServiceApiContextMut>|
-         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
-            let handler = handler.clone();
-            let context = request.state().clone();
-            request
-                .json()
-                .from_err()
-                .and_then(move |query: Q| {
-                    handler(&context, query)
-                        .map(|value| HttpResponse::Ok().json(value))
-                        .map_err(From::from)
-                })
-                .responder()
-        };
-
-        RequestHandler {
-            name: f.name,
-            method: actix_web::http::Method::POST,
-            inner: Box::new(index) as Box<RawHandler>,
-        }
-    }
-}
-
-impl<Q, I, F> From<NamedFn<ServiceApiContext, Q, I, Box<Future<Item = I, Error = error::Error>>, F>>
-    for RequestHandler
-where
-    F: for<'r> Fn(&'r ServiceApiContext, Q) -> Box<Future<Item = I, Error = error::Error>>
-        + 'static
-        + Clone,
-    Q: DeserializeOwned + 'static,
-    I: Serialize + 'static,
-{
-    fn from(
-        f: NamedFn<ServiceApiContext, Q, I, Box<Future<Item = I, Error = error::Error>>, F>,
-    ) -> Self {
-        let handler = f.inner.f;
-        let index = move |request: HttpRequest<ServiceApiContextMut>|
-         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
-            let context = request.state().clone();
-            let handler = handler.clone();
-            Query::from_request(&request, &())
-                .map(move |query: Query<Q>| query.into_inner())
-                .into_future()
-                .and_then(move |query| handler(&context, query).map_err(From::from))
-                .map(|value| HttpResponse::Ok().json(value))
-                .responder()
-        };
-
-        RequestHandler {
-            name: f.name,
-            method: actix_web::http::Method::GET,
-            inner: Box::new(index) as Box<RawHandler>,
-        }
-    }
-}
-
-impl<Q, I, F>
-    From<NamedFn<ServiceApiContextMut, Q, I, Box<Future<Item = I, Error = error::Error>>, F>>
-    for RequestHandler
-where
-    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> Box<Future<Item = I, Error = error::Error>>
-        + 'static
-        + Clone,
-    Q: DeserializeOwned + 'static,
-    I: Serialize + 'static,
-{
-    fn from(
-        f: NamedFn<ServiceApiContextMut, Q, I, Box<Future<Item = I, Error = error::Error>>, F>,
-    ) -> Self {
-        let handler = f.inner.f;
-        let index = move |request: HttpRequest<ServiceApiContextMut>|
-         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
-            let handler = handler.clone();
-            let context = request.state().clone();
-            request
-                .json()
-                .from_err()
-                .and_then(move |query: Q| {
-                    handler(&context, query)
-                        .map(|value| HttpResponse::Ok().json(value))
-                        .map_err(From::from)
-                })
-                .responder()
-        };
-
-        RequestHandler {
-            name: f.name,
-            method: actix_web::http::Method::POST,
-            inner: Box::new(index) as Box<RawHandler>,
-        }
-    }
 }
 
 #[derive(Default)]
@@ -166,5 +36,122 @@ impl BackendBuilder {
 
     pub fn finish(self) -> Vec<RequestHandler> {
         self.handlers
+    }
+}
+
+impl<Q, I, F> From<NamedFn<ServiceApiContext, Q, I, Result<I>, F>> for RequestHandler
+where
+    F: for<'r> Fn(&'r ServiceApiContext, Q) -> Result<I> + 'static,
+    Q: DeserializeOwned + 'static,
+    I: Serialize + 'static,
+{
+    fn from(f: NamedFn<ServiceApiContext, Q, I, Result<I>, F>) -> Self {
+        let handler = f.inner.f;
+        let index = move |request: HttpRequest<ServiceApiContextMut>|
+         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
+            let context = request.state();
+            let future = Query::from_request(&request, &())
+                .map(|query: Query<Q>| query.into_inner())
+                .and_then(|query| handler(context, query).map_err(From::from))
+                .and_then(|value| Ok(HttpResponse::Ok().json(value)))
+                .into_future();
+            Box::new(future)
+        };
+
+        RequestHandler {
+            name: f.name,
+            method: actix_web::http::Method::GET,
+            inner: Box::new(index) as Box<RawHandler>,
+        }
+    }
+}
+
+impl<Q, I, F> From<NamedFn<ServiceApiContextMut, Q, I, Result<I>, F>> for RequestHandler
+where
+    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> Result<I> + 'static + Clone,
+    Q: DeserializeOwned + 'static,
+    I: Serialize + 'static,
+{
+    fn from(f: NamedFn<ServiceApiContextMut, Q, I, Result<I>, F>) -> Self {
+        let handler = f.inner.f;
+        let index = move |request: HttpRequest<ServiceApiContextMut>|
+         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
+            let handler = handler.clone();
+            let context = request.state().clone();
+            request
+                .json()
+                .from_err()
+                .and_then(move |query: Q| {
+                    handler(&context, query)
+                        .map(|value| HttpResponse::Ok().json(value))
+                        .map_err(From::from)
+                })
+                .responder()
+        };
+
+        RequestHandler {
+            name: f.name,
+            method: actix_web::http::Method::POST,
+            inner: Box::new(index) as Box<RawHandler>,
+        }
+    }
+}
+
+impl<Q, I, F> From<NamedFn<ServiceApiContext, Q, I, AsyncResult<I>, F>> for RequestHandler
+where
+    F: for<'r> Fn(&'r ServiceApiContext, Q) -> AsyncResult<I> + 'static + Clone,
+    Q: DeserializeOwned + 'static,
+    I: Serialize + 'static,
+{
+    fn from(f: NamedFn<ServiceApiContext, Q, I, AsyncResult<I>, F>) -> Self {
+        let handler = f.inner.f;
+        let index = move |request: HttpRequest<ServiceApiContextMut>|
+         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
+            let context = request.state().clone();
+            let handler = handler.clone();
+            Query::from_request(&request, &())
+                .map(move |query: Query<Q>| query.into_inner())
+                .into_future()
+                .and_then(move |query| handler(&context, query).map_err(From::from))
+                .map(|value| HttpResponse::Ok().json(value))
+                .responder()
+        };
+
+        RequestHandler {
+            name: f.name,
+            method: actix_web::http::Method::GET,
+            inner: Box::new(index) as Box<RawHandler>,
+        }
+    }
+}
+
+impl<Q, I, F> From<NamedFn<ServiceApiContextMut, Q, I, AsyncResult<I>, F>> for RequestHandler
+where
+    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> AsyncResult<I> + 'static + Clone,
+    Q: DeserializeOwned + 'static,
+    I: Serialize + 'static,
+{
+    fn from(f: NamedFn<ServiceApiContextMut, Q, I, AsyncResult<I>, F>) -> Self {
+        let handler = f.inner.f;
+        let index = move |request: HttpRequest<ServiceApiContextMut>|
+         -> Box<Future<Item=HttpResponse, Error=actix_web::Error>> {
+            let handler = handler.clone();
+            let context = request.state().clone();
+            request
+                .json()
+                .from_err()
+                .and_then(move |query: Q| {
+                    handler(&context, query)
+                        .map(|value| HttpResponse::Ok().json(value))
+                        .map_err(From::from)
+                })
+                .responder()
+        };
+
+        RequestHandler {
+            name: f.name,
+            method: actix_web::http::Method::POST,
+            inner: Box::new(index) as Box<RawHandler>,
+        }
     }
 }
--- api-builder/src/lib.rs
+++ api-builder/src/lib.rs
@@ -13,8 +13,11 @@ pub mod actix_backend;
 pub mod error;
 pub mod service;
 
+pub type Result<I> = ::std::result::Result<I, error::Error>;
+pub type AsyncResult<I> = Box<Future<Item = I, Error = error::Error>>;
+
 pub struct TypedFn<S, Q, I, R, F> {
-    f: F,
+    pub(crate) f: F,
     _context_type: ::std::marker::PhantomData<S>,
     _query_type: ::std::marker::PhantomData<Q>,
     _item_type: ::std::marker::PhantomData<I>,
@@ -22,13 +25,13 @@ pub struct TypedFn<S, Q, I, R, F> {
 }
 
 pub struct NamedFn<S, Q, I, R, F> {
-    pub name: &'static str,
-    pub inner: TypedFn<S, Q, I, R, F>,
+    pub(crate) name: &'static str,
+    pub(crate) inner: TypedFn<S, Q, I, R, F>,
 }
 
-impl<Q, I, F> From<F> for TypedFn<ServiceApiContext, Q, I, Result<I, error::Error>, F>
+impl<Q, I, F> From<F> for TypedFn<ServiceApiContext, Q, I, Result<I>, F>
 where
-    F: for<'r> Fn(&'r ServiceApiContext, Q) -> Result<I, error::Error>,
+    F: for<'r> Fn(&'r ServiceApiContext, Q) -> Result<I>,
 {
     fn from(f: F) -> Self {
         TypedFn {
@@ -41,25 +44,9 @@ where
     }
 }
 
-impl<Q, I, F> From<F> for TypedFn<ServiceApiContextMut, Q, I, Result<I, error::Error>, F>
+impl<Q, I, F> From<F> for TypedFn<ServiceApiContextMut, Q, I, Result<I>, F>
 where
-    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> Result<I, error::Error>,
-{
-    fn from(f: F) -> Self {
-        TypedFn {
-            f,
-            _context_type: ::std::marker::PhantomData,
-            _query_type: ::std::marker::PhantomData,
-            _item_type: ::std::marker::PhantomData,
-            _result_type: ::std::marker::PhantomData,
-        }
-    }
-}
-
-impl<Q, I, F> From<F>
-    for TypedFn<ServiceApiContext, Q, I, Box<Future<Item = I, Error = error::Error>>, F>
-where
-    F: for<'r> Fn(&'r ServiceApiContext, Q) -> Box<Future<Item = I, Error = error::Error>>,
+    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> Result<I>,
 {
     fn from(f: F) -> Self {
         TypedFn {
@@ -73,9 +60,25 @@ where
 }
 
 impl<Q, I, F> From<F>
-    for TypedFn<ServiceApiContextMut, Q, I, Box<Future<Item = I, Error = error::Error>>, F>
+    for TypedFn<ServiceApiContext, Q, I, AsyncResult<I>, F>
 where
-    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> Box<Future<Item = I, Error = error::Error>>,
+    F: for<'r> Fn(&'r ServiceApiContext, Q) -> AsyncResult<I>,
+{
+    fn from(f: F) -> Self {
+        TypedFn {
+            f,
+            _context_type: ::std::marker::PhantomData,
+            _query_type: ::std::marker::PhantomData,
+            _item_type: ::std::marker::PhantomData,
+            _result_type: ::std::marker::PhantomData,
+        }
+    }
+}
+
+impl<Q, I, F> From<F>
+    for TypedFn<ServiceApiContextMut, Q, I, AsyncResult<I>, F>
+where
+    F: for<'r> Fn(&'r ServiceApiContextMut, Q) -> AsyncResult<I>,
 {
     fn from(f: F) -> Self {
         TypedFn {
